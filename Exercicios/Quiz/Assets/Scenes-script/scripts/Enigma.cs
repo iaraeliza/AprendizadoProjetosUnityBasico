67d24776a12bb7a61c67b6c4312cdcee987c11ef
Iara Eliza Souza Carvalho <iara.scarvalho@outlook.com>
2022-11-29 10:08:24 -0300

    att

---
+++ Exercicios/Quiz/Assets/Scenes-script/scripts/Enigma.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[System.Serializable]
+public class Enigma 
+{
+
+    [SerializeField] public string pergunta;
+    [SerializeField] public string respostaCorreta;
+    [SerializeField] public string respostaErrada1;
+    [SerializeField] public string respostaErrada2;
+    [SerializeField] public string respostaErrada3;
+
+
+
+
+
+}
